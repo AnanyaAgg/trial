88f60b1e0cd11828cf94b36c97dc2974eb4dd49c
AnaAgg1 <57201063+AnaAgg1@users.noreply.github.com>
2022-02-11 22:12:30 -0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Ananya Aggarwal on 2/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/trialApp.swift
@@ -0,0 +1,17 @@
+//
+//  trialApp.swift
+//  Shared
+//
+//  Created by Ananya Aggarwal on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct trialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
